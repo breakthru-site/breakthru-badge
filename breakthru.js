ba29da66eb5e5af6c15b8e5ddef6f691ece988e7
Druhin Tarafder <contact@druh.in>
2020-03-23 20:32:09 +0530

    Update breakthru.js

--- breakthru.js
+++ breakthru.js
@@ -1,2 +1,2 @@
 document.write("<style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');</style>");
-document.write('<style>img{position:absolute;bottom:0;right:2vw;position:fixed;margin-bottom:1vh;height:3.5vh;}</style><a target="_blank" href="https://breakthru.site"><img src="badge.svg"></a>');
+document.write('<style>img{position:absolute;bottom:0;right:2vw;position:fixed;margin-bottom:1vh;height:3.5vh;z-index:1000;}</style><a target="_blank" href="https://breakthru.site"><img src="badge.svg"></a>');
