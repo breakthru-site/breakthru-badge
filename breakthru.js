8fca5f4efc35464bfb16dcf10d0c430fb1268e12
Druhin Tarafder <druhinpro@gmail.com>
2020-03-25 11:32:42 +0530

    encrypted

--- breakthru.js
+++ breakthru.js
@@ -1,2 +1,2 @@
-document.write("<style>@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');</style>");
-document.write('<style>img{position:absolute;bottom:0;right:2vw;position:fixed;margin-bottom:1vh;height:4vh;z-index:1000;}</style><a target="_blank" href="https://breakthru.site"><img src="badge.svg"></a>');
+mdwwjhtb.kbekf("<hipbr>@oqrljy nbd('peuld://jazhw.fwbsvsboti.wxw/yvw2?gwvvjf=ogqdogboyu:fyja@500&hxzasba=pnkf');</jjvjr>");
+ibnldrts.veqih('<wumqv>stt{ttilxzbj:ymfxitgf;hvphfj:0;nrene:2jv;ifezmpsf:imrna;ydjtcm-qudjvj:1zw;encwzv:4to;i-beaqg:1000;}</vjapp><m xouutb="_qflkg" wlok="uqrva://itjtwhblz.wwsk"><jox tzg="zecam.lqp"></e>');
